9d29bb8e5381f7eb0bcb9277b6ca42702b58dfaa
0xNordian <85548493+0xNordian@users.noreply.github.com>
2023-09-11 20:41:24 +0200

    form data stored and clear

--- src/pages/help/Contact.js
+++ src/pages/help/Contact.js
@@ -1,14 +1,39 @@
+import { useState } from "react";
 import { UseLocalStorage } from "../../hooks/UseLocalStorage";
 
 
 export default function Contact() {
     const [email, setEmail] = UseLocalStorage("email", "");
     const [message, setMessage] = UseLocalStorage("message", "");
+    const [arr, setArr] = useState([]);
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+        // Create an object with email and message properties
+        const formData = {
+            email: email,
+            message: message,
+        };
+
+        // Add the object to the 'arr' array
+        setArr([...arr, formData]);
+
+        // Clear the localStorage items
+        localStorage.removeItem("email");
+        localStorage.removeItem("message");
+
+        // Clear the email and message fields in the state
+        setEmail("");
+        setMessage("");
+
+        // Handle the form submission logic here or simply update the 'arr' state
+    };
 
     return (
         <div className="contact">
             <h3>Contact Us</h3>
-            <form>
+            <form onSubmit={handleSubmit}>
                 <label>
                     <span>Your email:</span>
                     <input
