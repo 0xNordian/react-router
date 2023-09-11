7d58561a9a72da7b35686b2c9c647ce0b3625630
0xNordian <85548493+0xNordian@users.noreply.github.com>
2023-09-11 20:27:48 +0200

    useLocalStorage refactored into a custom hook

--- src/pages/help/Contact.js
+++ src/pages/help/Contact.js
@@ -1,41 +1,14 @@
-import { useState, useEffect } from "react";
+import { UseLocalStorage } from "../../hooks/UseLocalStorage";
+
 
 export default function Contact() {
-    const [email, setEmail] = useState("");
-    const [message, setMessage] = useState("");
-
-    useEffect(() => {
-        // Load data from localStorage when the component mounts
-        const storedData = JSON.parse(window.localStorage.getItem("contactData"));
-        if (storedData) {
-            setEmail(storedData.email || "");
-            setMessage(storedData.message || "");
-        }
-    }, []);
-
-    const setLocalStorage = (emailValue, msgValue) => {
-        try {
-            setEmail(emailValue);
-            setMessage(msgValue);
-
-            // Store both email and message as an object in localStorage
-            const contactData = { email: emailValue, message: msgValue };
-            window.localStorage.setItem("contactDatas", JSON.stringify(contactData));
-        } catch (error) {
-            console.error(error);
-        }
-    };
-
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        // Call setLocalStorage to update localStorage with the current values
-        setLocalStorage(email, message);
-    };
+    const [email, setEmail] = UseLocalStorage("email", "");
+    const [message, setMessage] = UseLocalStorage("message", "");
 
     return (
         <div className="contact">
             <h3>Contact Us</h3>
-            <form onSubmit={handleSubmit}>
+            <form>
                 <label>
                     <span>Your email:</span>
                     <input
